5506466b15309ee69cf2805ad51d10d685bc581c
kusshi <51787281+kusshi94@users.noreply.github.com>
2023-07-16 12:10:07 +0900

    :sparkles: add OUI struct to address information

--- cmd/proc.go
+++ cmd/proc.go
@@ -8,32 +8,33 @@ import (
 	"github.com/kusshi94/vendor6-cli/pkg/infra"
 )
 
+// ipToVendor returns the vendor name for the given IPv6 address.
 func ipToVendor(userInput string, db *infra.OUIDb) string {
-	// IPv6アドレスをパース
+	// Parse user input as IPv6 address
 	nip, err := netip.ParseAddr(userInput)
 	if err != nil {
 		return fmt.Sprintf("%s is not Valid IPv6 Address", userInput)
 	}
 
-	// IPv6かどうか判定
+	// Chek if the address is IPv6
 	if !nip.Is6() {
 		return fmt.Sprintf("%s is not IPv6 Adress", nip.String())
 	}
 
-	// IIDを抽出
+	// Get IID from IPv6 address
 	iid := getIID(nip)
 
-	// EUI-64判定
+	// Check if IID is EUI-64
 	if !iid.isEUI64() {
 		return fmt.Sprintf("%s is not EUI-64 Address", userInput)
 	}
 
-	// MACアドレスを取得
+	// Get MAC address from IID
 	mac := getMAC(iid)
 
-	// MACアドレスからOUI情報を取得
+	// Get OUI information from MAC address
 	oui := db.Lookup(mac)
-	// OUI情報を返す
+	// Return vendor name
 	if oui != nil {
 		return oui.Company
 	}
@@ -42,24 +43,26 @@ func ipToVendor(userInput string, db *infra.OUIDb) string {
 
 type IID [8]byte
 
-// IPアドレスからIIDを取り出す
+// getIID returns IID from IPv6 address.
 func getIID(nip netip.Addr) IID {
 	nip16 := nip.As16()
 	return IID(nip16[8:])
 }
 
+// isEUI64 returns true if IID is EUI-64.
 func (iid IID) isEUI64() bool {
-	// 中央にff:feが来るか？
+	// Check if IID is EUI-64
 	return iid[3] == 0xff && iid[4] == 0xfe
 }
 
-func getMAC(iid IID) net.HardwareAddr {
+// getMAC returns MAC address from EUI-64 IID.
+func getMAC(eui64 IID) net.HardwareAddr {
 	mac := make(net.HardwareAddr, 6)
-	mac[0] = iid[0] ^ 0x02
-	mac[1] = iid[1]
-	mac[2] = iid[2]
-	mac[3] = iid[5]
-	mac[4] = iid[6]
-	mac[5] = iid[7]
+	mac[0] = eui64[0] ^ 0x02
+	mac[1] = eui64[1]
+	mac[2] = eui64[2]
+	mac[3] = eui64[5]
+	mac[4] = eui64[6]
+	mac[5] = eui64[7]
 	return mac
 }
--- pkg/infra/ouidb.go
+++ pkg/infra/ouidb.go
@@ -16,31 +16,122 @@ import (
 )
 
 const (
-	// OUI一覧
+	// OUI_LIST_URL is the URL of the IEEE OUI list.
 	OUI_LIST_URL = "https://standards-oui.ieee.org/oui/oui.txt"
 )
 
+var (
+	// regular expressions for parsing oui.txt
+	pat_hex = regexp.MustCompile(`^([0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]) +\(hex\)\t\t(.*)$`)
+	pat_base16 = regexp.MustCompile(`^([0-9A-F]{6})     \(base 16\)\t\t(.*)$`)
+	pat_country = regexp.MustCompile(`^\t\t\t\t[A-Z][A-Z]$`)
+	pat_tab = regexp.MustCompile(`^\t\t\t\t`)
+)
+
+// OUI represents a single entry in the OUI list.
 type OUI struct {
-	Code    string
-	Company string
-	Country string
+	Code    string // OUI code (e.g. "38-9C-B2")
+	Company string // Company name (e.g. "Apple, Inc.")
+	Country string // Country code (e.g. "US")
+	Address string // Address (e.g. "1 Infinite Loop Cupertino CA 95014 US")
 }
 
+// String returns a string representation of the OUI.
+func (oui OUI) String() string {
+	return fmt.Sprintf("%s\t%s\t%s\t%s", oui.Code, oui.Company, oui.Country, oui.Address)
+}
+
+// OUI database
 type OUIDb struct {
 	ouimp map[string]OUI
 }
 
-// oui.txtを開く
+// Create a new OUI database from the given file.
+// If the file does not exist, it will be downloaded from the IEEE website.
+func NewOUIDb(ouiFilePath string) (*OUIDb, error) {
+	db := OUIDb{
+		ouimp: make(map[string]OUI),
+	}
+
+	// open oui.txt
+	// if the file does not exist, download it from the IEEE website
+	r, err := openOuiTxt(ouiFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	// open scanner
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+
+	// skip the first 4 lines
+	for i := 0; i < 4; i++ {
+		scanner.Scan()
+	}
+
+	// parse oui.txt line by line
+	newoui := OUI{}
+	for scanner.Scan() {
+		// get a line
+		line := scanner.Text()
+		switch {
+		case pat_hex.MatchString(line):
+			// hex format
+			// do nothing
+		case pat_base16.MatchString(line):
+			// base16 format
+			// e.g. "389CB2     (base 16)\t\tApple, Inc."
+			for i, s := range pat_base16.FindStringSubmatch(line) {
+				if i == 1 {
+					newoui.Code = strings.ToLower(s)
+				} else if i == 2 {
+					newoui.Company = s
+				}
+			}
+		case pat_country.MatchString(line):
+			// country code
+			newoui.Country = strings.TrimSpace(line)
+		case pat_tab.MatchString(line):
+			// address
+			if newoui.Address != "" {
+				newoui.Address += " "
+			}
+			newoui.Address += strings.TrimSpace(line)
+		case line == "":
+			// blank line (end of entry)
+			// add the entry to the database
+			if newoui.Code != "" {
+				db.ouimp[newoui.Code] = newoui
+				newoui = OUI{}
+			}
+		}
+	}
+
+	return &db, nil
+}
+
+// Lookup the given MAC address in the database and if found, return the OUI.
+func (db *OUIDb) Lookup(mac net.HardwareAddr) *OUI {
+	ret, ok := db.ouimp[strings.ReplaceAll(mac.String(), ":", "")[:6]]
+	if !ok {
+		return nil
+	}
+	return &ret
+}
+
+// Open oui.txt and return the file handle.
+// If the file does not exist, download it from the IEEE website.
 func openOuiTxt(ouiFilePath string) (io.ReadCloser, error) {
 	var err error
-	// oui.txtがなければダウンロードする
+	// If oui.txt does not exist, download it from the IEEE website.
 	if _, err = os.Stat(ouiFilePath); os.IsNotExist(err) {
 		err := fetchAndSaveOuiTxt(ouiFilePath)
 		if err != nil {
 			return nil, err
 		}
 	}
-	// oui.txtを開く
+	// open oui.txt
 	f, err := os.Open(ouiFilePath)
 	if err != nil {
 		return nil, err
@@ -48,95 +139,37 @@ func openOuiTxt(ouiFilePath string) (io.ReadCloser, error) {
 	return f, nil
 }
 
-// oui.txtをダウンロードして保存
+// Download oui.txt from the IEEE website and save it to the given file path.
 func fetchAndSaveOuiTxt(ouiFilePath string) error {
-	// UI表示
+	// Display a spinner while downloading
 	fmt.Println("Downloading oui.txt...")
 	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
 	s.Start()
+	defer s.Stop()
 
-	// oui.txtをダウンロード
+	// -- Download --
 	resp, err := http.Get(OUI_LIST_URL)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	// 保存
-	// ディレクトリがなければ作成
+	// -- Save to file --
+	// If the directory does not exist, create it.
 	err = os.MkdirAll(filepath.Dir(ouiFilePath), os.ModePerm)
 	if err != nil {
 		return err
 	}
-	// ファイル作成
+	// Create a file
 	file, err := os.Create(ouiFilePath)
 	if err != nil {
 		return err
 	}
-	// ファイルへのデータ書き込み
+	// Save
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
 		return err
 	}
 
-	s.Stop()
-
 	return nil
-}
-
-// OUIDbを作成
-func NewOUIDb(ouiFilePath string) (*OUIDb, error) {
-	db := OUIDb{
-		ouimp: make(map[string]OUI),
-	}
-
-	// oui.txtを開く
-	r, err := openOuiTxt(ouiFilePath)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Close()
-
-	// データを読み込む
-	pat_hex := regexp.MustCompile(`^([0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]) +\(hex\)\t\t(.*)$`)
-	pat_base16 := regexp.MustCompile(`^([0-9A-F]{6})     \(base 16\)\t\t(.*)$`)
-	pat_country := regexp.MustCompile(`^\t\t\t\t[A-Z][A-Z]$`)
-	pat_tab := regexp.MustCompile(`^\t\t\t\t`)
-
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanLines)
-
-	// 最初の4行は捨てる
-	for i := 0; i < 4; i++ {
-		scanner.Scan()
-	}
-
-	newoui := OUI{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		switch {
-		case pat_hex.MatchString(line):
-		case pat_base16.MatchString(line):
-			strs := pat_base16.FindStringSubmatch(line)
-			newoui.Code = strings.ToLower(strs[1])
-			newoui.Company = strs[2]
-		case pat_country.MatchString(line):
-			newoui.Country = strings.TrimSpace(line)
-		case pat_tab.MatchString(line):
-		case line == "":
-			db.ouimp[newoui.Code] = newoui
-			newoui = OUI{}
-		}
-	}
-
-	return &db, nil
-}
-
-// MACアドレスからOUIを検索
-func (db *OUIDb) Lookup(mac net.HardwareAddr) *OUI {
-	ret, ok := db.ouimp[strings.ReplaceAll(mac.String(), ":", "")[:6]]
-	if !ok {
-		return nil
-	}
-	return &ret
 }
